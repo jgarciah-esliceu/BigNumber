daa5a38c2763ac18823a39f58c4cfb17fdcabd29
jgarciah-esliceu <jgarciah@esliceu.net>
2023-02-06 16:36:51 +0100

    seguir multi

--- src/BigNumber.java
+++ src/BigNumber.java
@@ -16,15 +16,7 @@ class BigNumber {
     // Suma
     BigNumber add(BigNumber other) {
         // Afegeix zeros al numero que sigui de menor longitud
-        if (this.valor.length() > other.valor.length()) {
-            for (int i = other.valor.length(); i <= this.valor.length()-1; i++) {
-                other.valor = "0".concat(other.valor);
-            }
-        } else if (this.valor.length() < other.valor.length()) {
-            for (int i = this.valor.length(); i <= other.valor.length()-1; i++) {
-                this.valor = "0".concat(this.valor);
-            }
-        }
+        zerosIzq(other);
         int n1;
         int n2;
         int suma = 0;
@@ -40,10 +32,15 @@ class BigNumber {
             suma = n1 + n2;
 
             if (meLlevo >= 10){
+                suma++;
                 if (i == 0) {
-                    t = (suma + 1) + t;
+                    t = suma + t;
                 } else {
-                    t = (suma%10 + 1) + t;
+                    if (suma % 10 == 0) {
+                        t = 0 + t;
+                    } else {
+                        t = (suma%10) + t;
+                    }
                 }
             } else {
                 if (i == 0) {
@@ -59,12 +56,70 @@ class BigNumber {
 
     // Resta
     BigNumber sub(BigNumber other) {
-        return null;
+        zerosIzq(other);
+        int n1;
+        int n2;
+        int resta = 0;
+        String[] s1 = this.valor.split("");
+        String[] s2 = other.valor.split("");
+        BigNumber resultat = null;
+        String t = "";
+
+        for (int i = s1.length - 1; i >= 0; i--) {
+            n1 = Integer.parseInt(s1[i]);
+            n2 = Integer.parseInt(s2[i]);
+            int meLlevo = resta;
+            resta = n1 - n2;
+
+            if (meLlevo < 0) {
+                resta += 10;
+                resta--;
+                if (i == 0) {
+                    t = (resta%10) + t;
+                } else {
+                    if (resta % 10 == 0) {
+                        t = 0 + t;
+                    } else {
+                        t = (resta % 10) + t;
+                    }
+                }
+                resta -= 10;
+            } else if (i == 0) {
+                t = resta + t;
+            } else {
+                resta += 10;
+                t = (resta % 10) + t;
+                resta -= 10;
+            }
+            resultat = new BigNumber(t);
+        }
+        return resultat;
     }
+
+
 
     // Multiplica
     BigNumber mult(BigNumber other) {
-        return null;
+        zerosIzq(other);
+        int n1;
+        int n2;
+        int multiplicacio = 0;
+        String[] s1 = this.valor.split("");
+        String[] s2 = other.valor.split("");
+        BigNumber resultat = null;
+        String t = "";
+
+        for (int i = 0; i < s2.length; i++) {
+            n1 = Integer.parseInt(s1[i]);
+            n2 = Integer.parseInt(s2[i]);
+
+            multiplicacio = n1 + n1;
+            t = multiplicacio + t;
+
+            resultat = new BigNumber(t);
+        }
+
+        return resultat;
     }
 
     // Divideix
@@ -118,5 +173,19 @@ class BigNumber {
         BigNumber b = (BigNumber) other;
         if (b.valor.equals(this.valor)) return true;
         return false;
+    }
+
+    // Funciones propias
+    private void zerosIzq(BigNumber other) {
+        // Afegeix zeros al numero que sigui de menor longitud
+        if (this.valor.length() > other.valor.length()) {
+            for (int i = other.valor.length(); i <= this.valor.length()-1; i++) {
+                other.valor = "0".concat(other.valor);
+            }
+        } else if (this.valor.length() < other.valor.length()) {
+            for (int i = this.valor.length(); i <= other.valor.length()-1; i++) {
+                this.valor = "0".concat(this.valor);
+            }
+        }
     }
 }
